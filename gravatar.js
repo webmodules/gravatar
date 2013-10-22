8237c2b839e2b0cfe8e115e8c300d44e69c5f663
Guillermo Rauch <rauchg@gmail.com>
2013-10-21 22:31:21 -0500

    simplify

--- gravatar.js
+++ gravatar.js
@@ -24,28 +24,6 @@ exports.url = function (email, config) {
 };
 
 /**
- * Creates an avatar <img> element
- *
- * @param {String} email
- * @param {Number} size (80)
- * @return {Image} image dom
- * @api public
- */
-
-exports.img = function (email, config) {
-  config = config || {};
-  var size = config.s || config.size;
-  var url = exports.url(email, config);
-  var el = document.createElement('img');
-  el.setAttribute('src', url);
-  if (size) {
-    el.setAttribute('width', size);
-    el.setAttribute('height', size);
-  }
-  return el;
-};
-
-/**
  * Looks up a profile.
  *
  * @param {String} email
@@ -61,25 +39,6 @@ exports.profile = function (email, fn) {
       fn(null, obj.entry[0]);
     } else {
       fn(new Error('Bad response'));
-    }
-  });
-};
-
-/**
- * Shortcut to produce a username from an email.
- *
- * @param {String} email
- * @param {Function} callback
- * @api public
- */
-
-exports.username = function (email, fn) {
-  exports.profile(email, function (err, prof) {
-    if (err) return fn(err);
-    if (prof && prof.preferredUsername) {
-      fn(null, prof.preferredUsername);
-    } else {
-      fn(null, '');
     }
   });
 };
